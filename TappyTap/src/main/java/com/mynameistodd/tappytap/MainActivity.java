414e3249a902e9e04ff6bfb03e1169baff9e3a4a
Todd DeLand <todd.deland@gmail.com>
2013-10-17 07:48:42 -0400

    Move pager adapter to it's own class file. Update tab titles.

--- TappyTap/src/main/java/com/mynameistodd/tappytap/MainActivity.java
+++ TappyTap/src/main/java/com/mynameistodd/tappytap/MainActivity.java
@@ -14,23 +14,15 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 import java.io.IOException;
-import java.util.Locale;
 
 public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
 
@@ -69,7 +61,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the app.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(), context);
 
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.pager);
@@ -125,22 +117,25 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
                 }
             }
 
-            NdefRecord[] records = msgs[0].getRecords();
-            String payloadText = "";
-            for (int i=0;i<records.length;i++)
+            if (msgs != null && msgs.length > 0)
             {
-                if (records[i].getTnf() == NdefRecord.TNF_WELL_KNOWN && records[i].toMimeType() == "text/plain")
+                NdefRecord[] records = msgs[0].getRecords();
+                String payloadText = "";
+                for (int i=0;i<records.length;i++)
                 {
-                    byte[] payload = records[i].getPayload();
-                    String text = new String(payload);
-                    payloadText+=text;
+                    if (records[i].getTnf() == NdefRecord.TNF_WELL_KNOWN && records[i].toMimeType() == "text/plain")
+                    {
+                        byte[] payload = records[i].getPayload();
+                        String text = new String(payload);
+                        payloadText += text.substring(3);
+                    }
                 }
+                DialogFragment followDialog = new FollowDialogFragment();
+                Bundle args = new Bundle();
+                args.putString("followName", payloadText);
+                followDialog.setArguments(args);
+                followDialog.show(getSupportFragmentManager(),"followDialog");
             }
-            DialogFragment followDialog = new FollowDialogFragment();
-            Bundle args = new Bundle();
-            args.putString("followName", payloadText);
-            followDialog.setArguments(args);
-            followDialog.show(getSupportFragmentManager(),"followDialog");
         }
     }
 
@@ -164,81 +159,6 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 
     @Override
     public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a DummySectionFragment (defined as a static inner class
-            // below) with the page number as its lone argument.
-            Fragment fragment;
-            if (position < 1)
-            {
-                fragment = new ListMessages();
-            }
-            else
-            {
-                fragment = new DummySectionFragment();
-            }
-            Bundle args = new Bundle();
-            args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            Locale l = Locale.getDefault();
-            switch (position) {
-                case 0:
-                    return getString(R.string.title_section1).toUpperCase(l);
-                case 1:
-                    return getString(R.string.title_section2).toUpperCase(l);
-                case 2:
-                    return getString(R.string.title_section3).toUpperCase(l);
-            }
-            return null;
-        }
-    }
-
-    /**
-     * A dummy fragment representing a section of the app, but that simply
-     * displays dummy text.
-     */
-    public static class DummySectionFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        public static final String ARG_SECTION_NUMBER = "section_number";
-
-        public DummySectionFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main_dummy, container, false);
-            TextView dummyTextView = (TextView) rootView.findViewById(R.id.section_label);
-            dummyTextView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
     }
 
     /**
@@ -293,7 +213,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
             followName = getArguments().getString("followName");
 
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            builder.setMessage("Follow " + followName + "?")
+            builder.setMessage("Follow \"" + followName + "\"?")
                     .setPositiveButton("Yes",new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
@@ -303,7 +223,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
                     .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                            Toast.makeText(getActivity(),"Cancel",Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getActivity(), "Cancel", Toast.LENGTH_SHORT).show();
                         }
                     });
             return builder.create();
---
+++ TappyTap/src/main/java/com/mynameistodd/tappytap/SectionsPagerAdapter.java
@@ -0,0 +1,94 @@
+package com.mynameistodd.tappytap;
+
+/**
+ * Created by todd on 10/17/13.
+ */
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+/**
+ * A {@link android.support.v4.app.FragmentPagerAdapter} that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    private Context context;
+
+    public SectionsPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a DummySectionFragment (defined as a static inner class
+        // below) with the page number as its lone argument.
+        Fragment fragment = new ListMessages();
+        switch (position)
+        {
+            case 0:
+                fragment = new ListMessages();
+                break;
+            case 1:
+                fragment = new DummySectionFragment();
+                break;
+        }
+        Bundle args = new Bundle();
+        args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        Locale l = Locale.getDefault();
+        switch (position) {
+            case 0:
+                return context.getString(R.string.title_section1).toUpperCase(l);
+            case 1:
+                return context.getString(R.string.title_section2).toUpperCase(l);
+        }
+        return null;
+    }
+
+    /**
+     * A dummy fragment representing a section of the app, but that simply
+     * displays dummy text.
+     */
+    public static class DummySectionFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        public static final String ARG_SECTION_NUMBER = "section_number";
+
+        public DummySectionFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main_dummy, container, false);
+            TextView dummyTextView = (TextView) rootView.findViewById(R.id.section_label);
+            dummyTextView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
+}
